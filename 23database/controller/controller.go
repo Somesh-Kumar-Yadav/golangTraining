a9aa37c34bf1161057ab596454892e64c3c511ea
Somesh <someshkumar71524@gmail.com>
2023-04-29 23:28:16 +0530

    Added database

--- 23database/controller/controller.go
+++ 23database/controller/controller.go
@@ -3,9 +3,12 @@ package controller
 import (
 	"context"
 	"database/model"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -62,4 +65,90 @@ func updateOneMovie(movieId string) {
 		log.Fatal(err)
 	}
 	fmt.Println(result.ModifiedCount)
+}
+
+// delete 1 record
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	result, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result)
+}
+
+// delete all records from mongoDB
+
+func deleteAllMovie() int64 {
+	filter := bson.D{{}}
+	result, err := collection.DeleteMany(context.Background(), filter, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result.DeletedCount)
+	return result.DeletedCount
+}
+
+// get all movies from DB
+
+func getAllMovies() []primitive.M {
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer cursor.Close(context.Background())
+	var movies []primitive.M
+	for cursor.Next(context.Background()) {
+		var movie bson.M
+		err := cursor.Decode(&movie)
+		if err != nil {
+			log.Fatal(err)
+		}
+		movies = append(movies, movie)
+	}
+	return movies
+}
+
+// Actual controller
+
+func GetMyAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	allMovies := getAllMovies()
+	json.NewEncoder(w).Encode(allMovies)
+}
+
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+	var movie model.Netflix
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	insertOneMovie(movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func MarkAsWatched(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "PUT")
+
+	params := mux.Vars(r)
+	updateOneMovie(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+
+}
+
+func DeleteOneMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "DELETE")
+	params := mux.Vars(r)
+	deleteOneMovie(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+}
+
+func DeleteAllMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "DELETE")
+	var count int64 = deleteAllMovie()
+	json.NewEncoder(w).Encode(count)
 }
--- 23database/main.go
+++ 23database/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"database/router"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("MongoDb API")
+	r := router.Router()
+	fmt.Println("Server is getting started ....")
+
+	log.Fatal(http.ListenAndServe(":4000", r))
+	fmt.Println("Listen on port 4000")
 }
